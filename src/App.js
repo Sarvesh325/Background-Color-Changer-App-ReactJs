e611fe99f85017c0cc97bc2133dba764d0bf60c0
Sarvesh Bhagwat <137154776+Sarvesh325@users.noreply.github.com>
2023-10-28 11:08:19 +0530

    Background and Buttons Created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container w-100 h-screen">
+      <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2 ">
+        <button className="btn btn-primary flex flex-wrap justify-center me-2 shadow-lg  px-3 py-2 rounded-3xl">
+          BLUE
+        </button>
+        <button className="btn btn-danger flex flex-wrap justify-center me-2 shadow-lg  px-3 py-2 rounded-3xl">
+          RED
+        </button>
+        <button className="btn btn-warning flex flex-wrap justify-center me-2 shadow-lg  px-3 py-2 rounded-3xl">
+          ORANGE
+        </button>
+        <button className="btn btn-success flex flex-wrap justify-center me-2 shadow-lg  px-3 py-2 rounded-3xl">
+          GREEN
+        </button>
+        <button className="btn btn-secondary flex flex-wrap justify-center me-2 shadow-lg  px-3 py-2 rounded-3xl">
+          GREY
+        </button>
+        <button className="btn btn-dark flex flex-wrap justify-center me-2 shadow-lg  px-3 py-2 rounded-3xl">
+          DARK
+        </button>
+        <button className="btn btn-light flex flex-wrap justify-center me-2 shadow-lg  px-3 py-2 rounded-3xl">
+          WHITE
+        </button>
+      </div>
     </div>
   );
 }
